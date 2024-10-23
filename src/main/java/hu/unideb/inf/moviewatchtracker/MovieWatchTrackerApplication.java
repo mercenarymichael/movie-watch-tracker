cde7dbd95af6d6b3807a4f677c8c97daeae78a8a
Gréczi-Zsoldos Miklós <miklos.greczi.zsoldos@gmail.com>
2024-10-23 11:38:13 +0200

    initial commit

---
+++ src/main/java/hu/unideb/inf/moviewatchtracker/MovieWatchTrackerApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.moviewatchtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWatchTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieWatchTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/unideb/inf/moviewatchtracker/MovieWatchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.moviewatchtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
